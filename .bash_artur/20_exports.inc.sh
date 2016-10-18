771b096740b243c6dd507caa417f65ef735cbc5f
Artur Drozd <nwkj86@gmail.com>
2016-10-18 23:38:30 +0100

    Updates, snippets mostly

--- .bash_artur/20_exports.inc.sh
+++ .bash_artur/20_exports.inc.sh
@@ -31,13 +31,6 @@ export HISTSIZE=1024
 ###
 export SAL_USE_VCLPLUGIN=gtk
 
-###
-# Compilation *FLAGS
-###
-export CFLAGS="-march=native -mtune=native -O2 -pipe -fstack-protector-strong --param=ssp-buffer-size=4"
-export CXXFLAGS=$CFLAGS
-export MAKEFLAGS="-j4"
-
 # footer
 unit_name="$(basename $BASH_SOURCE)"
 echo -n " -> $unit_name"
