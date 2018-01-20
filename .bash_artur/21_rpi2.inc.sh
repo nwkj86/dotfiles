1b664ccd284bc2d5680f5b5c8c0319060e7d28aa
Artur Drozd <nwkj86@gmail.com>
2018-01-20 17:19:10 +0000

    Cleanup. Move rpi2 related config to separate file

--- .bash_artur/21_rpi2.inc.sh
+++ .bash_artur/21_rpi2.inc.sh
@@ -4,8 +4,17 @@
 
 # remote to rpi2
 alias rpi2_x11="Xnest -query rpi2 -geometry 800x600 :2"
-# (u)mount rpi2 resources
-alias rpi2_mount="mount ~/rpi2/public;"
-alias rpi2_umount="umount ~/rpi2/public;"
+
+function rpi2_mount()
+{
+    mount ~/rpi2/public
+    mount ~/rpi2/artur2
+}
+
+function rpi2_umount()
+{
+    umount ~/rpi2/artur2
+    umount ~/rpi2/public
+}
 
 print_footer $BASH_SOURCE
