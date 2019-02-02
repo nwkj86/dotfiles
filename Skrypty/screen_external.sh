ca0b9b090adb3b66c0c076f20d63886ecfb9c92c
Artur Drozd <nwkj86@gmail.com>
2019-02-02 11:17:05 +0000

    Rework internal/external screen switching

--- Skrypty/screen_external.sh
+++ Skrypty/screen_external.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
 xrandr --output eDP-1 --off \
-    --output HDMI-1 --auto --scale 1.8x1.8 --panning 3456x1944+0+0 \
-    --output DP-2 --auto --scale 1.8x1.8 --panning 3456x1944+3456+0 \
-    --fb 6912x1944
+    --output DP-2 --auto \
+    --output HDMI-1 --auto --right-of DP-2 \
 
 # Sometimes doesn't work for the first time, so retry
-xrandr --output eDP-1 --off \
-    --output HDMI-1 --auto --scale 1.8x1.8 --panning 3456x1944+0+0 \
-    --output DP-2 --auto --scale 1.8x1.8 --panning 3456x1944+3456+0 \
-    --fb 6912x1944
 
-killall plasmashell
-sleep 1
-plasmashell > /dev/null  2> /dev/null&
+#killall plasmashell
+#sleep 1
+#plasmashell > /dev/null  2> /dev/null&
--- Skrypty/screen_internal.sh
+++ Skrypty/screen_internal.sh
@@ -4,7 +4,7 @@ xrandr --output eDP-1 --auto \
     --output HDMI-1 --off \
     --output DP-2 --off
 
-killall plasmashell
-sleep 1
-plasmashell > /dev/null  2> /dev/null&
+#killall plasmashell
+#sleep 1
+#plasmashell > /dev/null  2> /dev/null&
 
